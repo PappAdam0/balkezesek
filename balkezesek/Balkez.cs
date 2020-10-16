aaf8776dc83ed873b33bd0c936b534869c2873bf
PappAdam0 <papp.adaam@gmail.com>
2020-10-16 12:36:30 +0200

    Add project files.

---
+++ balkezesek/Balkez.cs
@@ -0,0 +1,21 @@
+﻿namespace balkezesek
+{
+    internal class Balkez
+    {
+
+        public string Nev { get; private set; }
+        public string Elso { get; private set; }
+        public string Utolso { get; private set; }
+        public int Suly { get; private set; }
+        public int Magassag { get; private set; }
+        public Balkez(string sor)
+        {
+            string[] adat = sor.Split(';');
+            Nev = adat[0];
+            Elso = adat[1];
+            Utolso = adat[2];
+            Suly = int.Parse(adat[3]);
+            Magassag = int.Parse(adat[4]);
+        }
+    }
+}
---
+++ balkezesek/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.ComponentModel;
+
+namespace balkezesek
+{
+    class Program
+    {
+        static List<Balkez> balkez = new List<Balkez>();
+        static Dictionary<string, int> utoljara = new Dictionary<string, int>();
+        static int evszam = 0;
+        static void Beolvas()
+        {
+            StreamReader be = new StreamReader("balkezesek.csv");
+            be.ReadLine();
+            while (!be.EndOfStream)
+            {
+                balkez.Add(new Balkez(be.ReadLine()));
+            }
+            be.Close();
+            Console.WriteLine($"3.Feladat: {balkez.Count}");
+        }
+
+        static void Negyedik()
+        {
+            Console.WriteLine("4.Feladat:");
+            foreach (var i in balkez)
+            {
+                if (i.Utolso.Contains("1999-10"))
+                {
+                    utoljara.Add(i.Nev, i.Magassag);
+                }
+            }
+
+            foreach (var i in utoljara)
+            {
+                Console.WriteLine($"\t{i.Key}, {Math.Round(i.Value*2.54,1)}");
+            }
+        }
+
+        static void Otodik()
+        {
+            while (true)
+            {
+                Console.WriteLine("5. Feladat: ");
+                Console.Write("Kérek egy 1990 és 1999 közötti évszámot: ");
+                evszam = int.Parse(Console.ReadLine());
+                if (evszam >= 1900 && evszam <= 1999)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.Write("Hibás adat! Kérek egy 1990 és 1999 közötti évszámot: ");
+
+                }
+            }
+        }
+
+        static void Hatodik()
+        {
+            foreach (var i in balkez)
+            {
+                if (evszam => i.Elso)
+                {
+
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Beolvas();
+            Negyedik();
+            Otodik();
+            Hatodik();
+            Console.ReadKey();
+        }
+    }
+}
